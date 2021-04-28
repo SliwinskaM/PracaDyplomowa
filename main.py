79d0675b13aa0ccca7086468251ea5689e109ca3
SliwinskaM <63351894+SliwinskaM@users.noreply.github.com>
2021-04-28 09:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ import fuzzy_curves as fc
 import recommend as re
 import visualizations as vs
 import numpy as np
+from statistics import mean
 
 # read data
-data = import_data.ImportData('smoker')
-data.import_data()
+data = import_data.ImportData('movies_short')
+# data.import_data()
+data.import_movies_genres()
 
 #choose fuzzy curves class
 curves = fc.Curves1(data.min_score, data.max_score, 0.2, 0.45, 0.55, 0.8)
 
 # # create fuzzy association rules
 conv_r_matrix = create_converted_r_matrix(data.r_matrix, curves)
+# vs.write_r_matrix(data, conv_r_matrix, curves)
 
 # apriori1 = apr.Apriori(conv_r_matrix, curves.Names, 0.0052)
 # freq1, sup1 = apriori1.apriori()
@@ -33,6 +36,16 @@ conv_r_matrix = create_converted_r_matrix(data.r_matrix, curves)
 # vs.write_rules(data, rules2, conf2, supru2, curves)
 
 recomm = re.Recommend(conv_r_matrix)
-recomm_score, map_3, map_5, map_10 = recomm.main_recommend(20, curves.Names, test_size=0.3, cross_num=10, min_support=0.0002, min_confidence=0.000005)
+recomm_score, map_3, map_5, map_7, map_10, map_20 = recomm.main_recommend(20, curves.Names, test_size=0.3, cross_num=10, min_support=0.3, min_confidence=0.15)
+print(map_3)
+print(mean(map_3))
+print(map_5)
+print(mean(map_5))
+print(map_7)
+print(mean(map_7))
+print(map_10)
+print(mean(map_10))
+print(map_20)
+print(mean(map_20))
 vs.write_recomms(data, recomm_score)
 pass
